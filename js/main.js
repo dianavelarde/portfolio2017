dffea5aa159567156fa544a09cf966304e2f64fa
dianavelarde <van_di09@hotmail.com>
2017-01-11 14:35:27 -0500

    Insert favicon

--- js/main.js
+++ js/main.js
@@ -1,24 +1,44 @@
-		var w = $(window).width(),
-    toggle 		= $('#toggle-menu'),
-    menu 		= $('nav ul'),
-    hasChild = $('.has-child'),
-    dropdown = $('.dropdown');
 
-$(function() {
-  $(toggle).on('click', function(e) {
-    e.preventDefault();
-    menu.toggle();
-  });
-  
-  $(hasChild).click(function(e) {
-    e.preventDefault();
-    dropdown.toggle();
-  });
+$(document).ready(function() {
+        function getTargetTop(elem){
+            var id = elem.attr("href");
+            var offset = 10;
+            return $(id).offset().top - offset;
+        }
+        $('a[href^="#"]').click(function (event) {
+            var target = getTargetTop($(this));
+            $('html, body').animate({scrollTop:target}, 1100);
+            event.preventDefault();
+        });
+        var sections = $('a[href^="#"]');
+        function checkSectionSelected(scrolledTo){
+            var threshold = 150;
+            var i;
+            for (i = 0; i < sections.length; i++) {
+                var section = $(sections[i]);
+                var target = getTargetTop(section);
+                if (scrolledTo > target - threshold && scrolledTo < target + threshold) {
+                    sections.removeClass("active");
+                    section.addClass("active");
+                }
+            };
+        }
+        checkSectionSelected($(window).scrollTop());
+        $(window).scroll(function (e){
+            checkSectionSelected($(window).scrollTop())
+        });
+        $('.navigation').prepend('<div id="menu-icon"></div>');
+        $("#menu-icon").on("click", function(){
+          if ( $(document).width() < 581 ) {
+            $(".nav").slideToggle();
+            $(this).toggleClass("active");
+          }            
+        });
+        $('.nav li a').on('click', function() {
+          console.log($(document).width());
+          if ($(document).width() < 581) {
+            $('.nav').slideToggle();
+            $('.nav').toggleClass('active');
+          }
+        }); 
 });
-
-$(window).resize(function(){
-  if(w > 320 && menu.is(':hidden')) {
-    menu.removeAttr('style');}
-});
-
-
